61207e2348352d7bfe98e79263cd458b8330f7ee
Hassan Elgamily <hassanelgamily@gmail.com>
2024-09-19 19:59:49 +0200

    create Task model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Task(models.Model):
+    creator = models.ForeignKey(User, related_name='created_tasks', on_delete=models.CASCADE)
+    executor = models.ForeignKey(User, related_name='executed_tasks', on_delete=models.SET_NULL, null=True, blank=True)
+    name = models.CharField(max_length=255)
+    cost = models.DecimalField(max_digits=8, decimal_places=2)
+    is_done = models.BooleanField(default=False)
+    deadline = models.DateTimeField()
+
+    def __str__(self):
+        return self.name
