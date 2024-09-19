de7322383798752a4b0677984ef6163f607842d5
Hassan <hassanelgamily@gmail.com>
2024-09-19 18:20:24 +0200

    add views

--- app/views.py
+++ app/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
 
-# Create your views here.
+#Developer 1 Views:
+class UserCreateView():
+    pass
+
+class LoginView():
+    pass
+
+class LogoutView():
+    pass
+
+class TaskCreateView():
+    pass
+
+class TasksCreatedByUser():
+    pass
+
+class TaskWithExecutorAPIView():
+    pass
+
+#Developer 2 Views:
+class UserTasksAPIView():
+    pass
+
+class UserTasksStatsAPIView():
+    pass
+
+class UnassignedTasksAPIView():
+    pass
+
+class BecomeExecutorAPIView():
+    pass
+
+class MarkTaskDoneAPIView():
+    pass
