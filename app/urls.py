91c10c3f7c71b5e5dde15dc441538477ea75a38d
Hassan Elgamily <hassanelgamily@gmail.com>
2024-10-14 20:20:28 +0200

    modify urls.py

--- app/urls.py
+++ app/urls.py
@@ -2,14 +2,14 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    # path('user/create/', UserCreateView.as_view(), name='user_create'),
-    # path('login/', LoginView.as_view(), name='login'),
-    # path('logout/', LogoutView.as_view(), name='logout'),
-    # path('task/create/', TaskCreateView.as_view(), name='task_create'),
-    # path('tasks-created-by-user/', TasksCreatedByUser.as_view(), name='user_tasks'),
+    path('user/create/', UserCreateView.as_view(), name='user_create'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('task/create/', TaskCreateView.as_view(), name='task_create'),
+    path('tasks-created-by-user/', TasksCreatedByUser.as_view(), name='user_tasks'),
     path('user-tasks-stats/', UserTasksStatsAPIView.as_view(), name='user-tasks-stats'),
     path('unassigned-tasks/', UnassignedTasksAPIView.as_view(), name='unassigned-tasks'),
-    # path('task/executor/', TaskWithExecutorAPIView.as_view(), name='task_executor'),
+    path('task/executor/', TaskWithExecutorAPIView.as_view(), name='task_executor'),
     path('user-tasks/', UserTasksAPIView.as_view(), name='my-tasks'),
     path('become-executor/<int:task_id>/', BecomeExecutorAPIView.as_view(), name='become-executor'),
     path('mark-task-done/<int:task_id>/', MarkTaskDoneAPIView.as_view(), name='mark-task-done'),
